ddba1462d1119c92fe0a55b419507d5831f36e83
0xDonatHack <0xDonatHack>
2024-06-29 12:05:44 +0530

    Rendering html Templstes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>hello, World!</h1>"
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
